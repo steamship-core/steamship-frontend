d354d4be2046125a98d785abf9675989515be9d8
Max Freundlich <mfreundlich1@gmail.com>
2023-08-04 12:41:34 -0400

    fix: remove src dir from bundle, fix type defs

---
+++ .changeset/early-penguins-ring.md
@@ -0,0 +1,5 @@
+---
+"@steamship/react": patch
+---
+
+remove src dir from bundle
--- packages/react-components/tsup.config.ts
+++ packages/react-components/tsup.config.ts
@@ -2,10 +2,14 @@ import { Options } from "tsup";
 
 const options: Options = {
   format: ["cjs", "esm"], // generate cjs and esm files
-  clean: false,
+  clean: true,
   splitting: false,
   dts: true,
-  entryPoints: ["index.tsx", "src/next/server.ts", "src/next/client.ts"],
+  entry: {
+    index: "index.tsx",
+    "next/server": "src/next/server.ts",
+    "next/client": "src/next/client.ts",
+  },
   minify: true,
   sourcemap: true,
   target: "es2020",
